edb7855fe4ecabe7e8f46a913c7143d1a5618f95
engahmed10 <eng.computer1@yahoo.com>
2020-07-19 23:19:18 -0700

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,17 @@ Rails.application.routes.draw do
 
   resources :posts, only: [:index, :show, :new, :create, :edit, :update]
 
+  #get 'authors/:id/posts', to: 'authors#posts_index'
+  #get 'authors/:id/posts/:post_id', to: 'authors#post'
+
+
+
+  resources :authors, only: [:show]  do 
+   resources :posts, only: [:show, :index]
+  end
+
+  resources :posts, only: [:index,:show,:new,:create,:edit,:update]
+
   root 'posts#index'
 
 end
